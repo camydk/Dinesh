5b330a5e4bb62aade4ce8764fe9a0e3c2e20b629
camydk <92867734+camydk@users.noreply.github.com>
2022-03-04 19:13:42 +0530

    Update WebApp.py

--- WebApp.py
+++ WebApp.py
@@ -2,8 +2,10 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 class Guna(BaseModel):
     age=19
-    reg=6117
-    
+    Date of birth=06-05-2003
+    College code=6117
+    Register number=611720104022
+
 nsit=FastAPI()
 
 @nsit.get("/{Name:path}")
@@ -11,4 +13,4 @@ def guna(Name:str):
      return{"Narasu's Sarathy Institute Of Technology":Name}
 
 @nsit.post("/item")
-def reg(i:Guna):return(i)
+def reg(i: Dinesh):return(i)
