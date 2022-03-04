049e33da715689a9766630d357de12cfc0a6ccd2
camydk <92867734+camydk@users.noreply.github.com>
2022-03-04 19:42:46 +0530

    Update WebApp.py

--- WebApp.py
+++ WebApp.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 class Dinesh(BaseModel):
     age=19
-    Date of birth=06-05-2003
+    Date of birth=26-01-2003
     College code=6117
     Register number=611720104022
 
